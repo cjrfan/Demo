e5c669d508d072dcde955d9e34d173d5efa948df
cjrfan <2298579045@qq.com>
2016-03-19 17:16:14 +0800

    js demo
    js demo

---
+++ miaov/js/data.js
@@ -0,0 +1,222 @@
+var data = [{
+    'title': '第一课 - 属性操作、图片切换、短信发送模拟',
+    'content': [{
+        'text': '热身练习1',
+        'link': 'Basic/01_change_div_style.html'
+    }, {
+        'text': '热身练习2',
+        'link': 'Basic/02_change_div_style2.html'
+    }, {
+        'text': '图片切换综合实例',
+        'link': 'Basic/03_switch_images.html'
+    }, {
+        'text': '模拟手机短信发送',
+        'link': 'Basic/04_send_msg.html'
+    }]
+}, {
+    'title': '第二课 -  for应用this关键字',
+    'content': [{
+        'text': '鼠标悬浮提示',
+        'link': 'Basic/05_mouse_hover_tip.html'
+    }, {
+        'text': 'for图片生成应用',
+        'link': 'Basic/06_for_pic_generate.html'
+    }, {
+        'text': '自动生成100个div，每10个换行',
+        'link': 'Basic/07_auto_make_100div.html'
+    }, {
+        'text': '点击生成V形',
+        'link': 'Basic/09_click_make_v.html'
+    }, {
+        'text': '自动生成div（综合）',
+        'link': 'Basic/10_make_div.html'
+    }]
+}/*, {
+    'title': '第三课 - 自定义属性、索引值',
+    'content': [{
+        'text': 'QQ列表展开收缩扩展',
+        'link': 'Basic/6-QQ-friend-list.html'
+    }, {
+        'text': '带缩略图的图片切换效果',
+        'link': 'Basic/7-switch-imgs-has-thumbnail.html'
+    }, {
+        'text': '控制多组图片切换',
+        'link': 'Basic/8-multiple-switch-imgs.html'
+    }]
+}, {
+    'title': '第四课 - JS数据类型、类型转换',
+    'content': [{
+        'text': '找到合适的值',
+        'link': 'Basic/9-transform-arr-values.html'
+    }, {
+        'text': '过滤QQ号码',
+        'link': 'Basic/10-QQ-account-filter.html'
+    }, {
+        'text': '控制多组图片切换',
+        'link': 'Basic/8-multiple-switch-imgs.html'
+    }]
+}, {
+    'title': '第五课 - 函数传参、重用、价格计算',
+    'content': [{
+        'text': '搜狐视频记录片列表展示',
+        'link': 'Basic/11-news-tab-lists-sohu/'
+    }, {
+        'text': '修改文本框的值',
+        'link': 'Basic/12-multi-category-edit.html'
+    }]
+}, {
+    'title': '第六课 - 作用域（无练习）',
+}, {
+    'title': '第七课 - 运算符流程控制',
+    'content': [{
+        'text': '淘宝星星评分',
+        'link': 'Basic/14-star-rating/'
+    }, {
+        'text': '全选操作与反选操作',
+        'link': 'Basic/13-multiple-checkbox.html'
+    }]
+}, {
+    'title': '第八课 - return定时器基础',
+    'content': [{
+        'text': '按钮控制容器内容上下滚动',
+        'link': 'Basic/16-scroll-button-control.html'
+    }, {
+        'text': '延时消失的菜单',
+        'link': 'Basic/15-settimeout-nav.html'
+    }, {
+        'text': '自动轮换选项卡',
+        'link': 'Basic/17-auto-change-tabs/'
+    }]
+}, {
+    'title': '第九课 - 定时器管理、函数封装',
+    'content': [{
+        'text': '运动队列-依次落下又返回',
+        'link': 'Basic/19-queue-action-callback.html'
+    }, {
+        'text': '修复shake“抖”函数的bug',
+        'link': 'Basic/12-multi-category-edit.html'
+    }, {
+        'text': '点击加分效果-可重复点击',
+        'link': 'Basic/23-auto-slider.html'
+    }, {
+        'text': '封装透明度函数',
+        'link': 'Basic/20-opacity-function.html'
+    }]
+}, {
+    'title': '第十课 - 日期对象、时钟倒计时',
+    'content': [{
+        'text': '倒计时抢购商品',
+        'link': 'Basic/27-flash-sale/'
+    }, {
+        'text': '图片时间',
+        'link': 'Basic/26-pic-clock(advanced)/25-pic-clock-basic.html'
+    }, {
+        'text': '图片时间-扩展版',
+        'link': 'Basic/26-pic-clock(advanced)/26-pic-clock(advanced).html'
+    }]
+}, {
+    'title': '第十一课 - 字符串、查找高亮显示',
+    'content': [{
+        'text': '查找、替换、删除',
+        'link': 'Basic/28-search-replace-delete.html'
+    }, {
+        'text': '文字搬运工',
+        'link': 'Basic/29-text-transform.html'
+    }]
+}, {
+    'title': '第十二课 - 数组随机、数组去重',
+    'content': [{
+        'text': '随机图片滚动',
+        'link': 'Basic/32-random-pic-change/'
+    }, {
+        'text': '自定义字体形状',
+        'link': 'Basic/31-diy-font-style/'
+    }, {
+        'text': '排序',
+        'link': 'Basic/30-sort/'
+    }]
+}*/, {
+    'title': '附---函数封装',
+    'content': [{
+        'text': '函数封装',
+        'link': 'Basic/Cjrfan.js'
+    }]
+}, {
+    'title': '第一课 - DOM基础概念、操作',
+    'content': [{
+        'text': '留言本，限制条数',
+        'link': 'Primary/1-1.html'
+    }]
+}, {
+    'title': '第二课 - DOM、BOM相关方法及属性',
+    'content': [{
+        'text': '关键字title提示',
+        'link': 'Primary/2-1.html'
+    }, {
+        'text': '回到顶部',
+        'link': 'Primary/2-2.html'
+    }, {
+        'text': '数据操作+弹出层提示',
+        'link': 'Primary/2-3.html'
+    }, {
+        'text': '选择筛选',
+        'link': 'Primary/2-4.html'
+    }]
+}, {
+    'title': '第三课 - Event-事件详解',
+    'content': [{
+        'text': '键盘控制div移动',
+        'link': 'Primary/3-1.html'
+    }, {
+        'text': '模拟select单选',
+        'link': 'Primary/3-2.html'
+    }, {
+        'text': '模拟select多选',
+        'link': 'Primary/3-3.html'
+    }]
+}, {
+    'title': '第四课 - 事件深入应用',
+    'content': [{
+        'text': '鼠标绘制方块',
+        'link': 'Primary/4-3.html'
+    }, {
+        'text': '拖拽回放',
+        'link': 'Primary/4-4.html'
+    }]
+}, {
+    'title': '原生JS实例',
+    'content': [{
+        'text': '自定义滚动条',
+        'link': 'Practice/1.html'
+    }, {
+        'text': '方块碰撞检测变颜色',
+        'link': 'Practice/2.html'
+    }, {
+        'text': '放大镜',
+        'link': 'Practice/3.html'
+    }, {
+        'text': '拖拽效果',
+        'link': 'Practice/5.html'
+    }, {
+        'text': '右键菜单',
+        'link': 'Practice/6.html'
+    }, {
+        'text': '微博发文键盘事件',
+        'link': 'Practice/7.html'
+    }, {
+        'text': '查找替换、替换全部',
+        'link': 'Practice/8.html'
+    }, {
+        'text': '展开收缩',
+        'link': 'Practice/9.html'
+    }, {
+        'text': '简单瀑布流',
+        'link': 'Practice/10.html'
+    }, {
+        'text': '方块遇障碍物反弹',
+        'link': 'Practice/11.html'
+    }, {
+        'text': '方块遇2障碍物反弹',
+        'link': 'Practice/12.html'
+    }]
+}];
